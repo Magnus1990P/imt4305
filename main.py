c2ee3dd01c42ed038339a64841f45f80ce0d9247
Magnus Øverbø <magnus@maggz.nl>
2016-12-07 10:17:11 +0100

    Removed matlab and merged uc_regina code with main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from 	cv2						import *
-from numpy 					import *
-import sys
-import scipy 				as sp
-import scipy.misc 	as spMisc
-import scipy.signal	as spSignal
-import matplotlib.pyplot as plt
+from		cv2								import *
+from		numpy 						import *
+import	sys
+import	scipy 						as sp
+import	scipy.misc 				as spMisc
+import	scipy.signal			as spSignal
+import	matplotlib.pyplot as plt
+from		uc_regina_filter	import	*
 
 spHeight	= 1
 spWidth		= 1
@@ -16,6 +17,87 @@ filters		= ["Low-Pass","High-Pass",]
 genIMG		= {}
 metrics		= {}
 
+
+#This code in its entirety is collected and converted from the code published on
+#the University of Regina, Department of Computer Science's websites below
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/lpfilter.m
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/hpfilter.m
+#
+#Calculate DFTUV of MxN matrix
+# Generate two padded meshgrids
+#
+#	@param M:			Width
+#	@param N:			Height
+#
+# @return V:		Meshgrid of size MxN
+#	@return U:		Meshgrid of size MxN
+def dftuv(M, N):
+	idx	= idy	= 0
+	u = range(0,M)
+	v = range(0,N)
+	for idx in range(0, len(u)):			#Convert all elements greater than M/2
+		if u[idx] > (M/2):
+			u[idx] = u[idx] - M
+	for idy in range(0, len(v)):			#Convert all elements greater than N/2
+		if v[idy] > (N/2):
+			v[idy] = v[idy] - N
+	V,U = meshgrid(v, u)							#Generate meshgrid
+	return [V,U]											#Return meshgrid
+
+
+#This code in its entirety is collected and converted from the code published on
+#the University of Regina, Department of Computer Science's websites below
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/lpfilter.m
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/hpfilter.m
+#
+# Generate low pass filters of MxN size.
+#
+#	@param type:	Type of filter to generate. ideal, butterworth or gaussian
+# @param M:			Height of matrix
+# @param N:			Width of matrix
+# @param D0:		Cut-off frequency.  Positive float value
+# @param n:			multiplication value for butterworth, default 1.0 if not set
+#
+#	@return H:		matrix of MxN
+def genLPFilter(t, M, N, D0=100, n=1.0):
+	U,V = dftuv( M, N )										# Compute padded meshgrid
+	D = sqrt( power(U,2) + power(V,2) )		# square root of array
+	H = array(D)													# Convert to array
+	if t == "ideal":
+		for y in range(0, len(H)):
+			for x in range(0, len(H[y])):
+				H[y][x] = 1 if H[y][x]<=D0 else 0
+
+	elif t == "btw":
+		H = 1.0 / (1 + power((H/D0), (2*n)) )
+	
+	elif t == "gaussian":
+		H = exp( (-1*power(D,2)) / (2*(D0**2)) )
+	
+	else:
+		print "Unknown filter type, quitting: %s" % t
+		sys.exit(0)
+	return H
+
+
+#This code in its entirety is collected and converted from the code published on
+#the University of Regina, Department of Computer Science's websites below
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/lpfilter.m
+#http://www.cs.uregina.ca/Links/class-info/425-nova/Lab5/M-Functions/hpfilter.m
+#
+# Generates a high pass filters of MxN size.
+#
+#	@param type:	Type of filter to generate. ideal, butterworth or gaussian
+# @param M:			Height of matrix
+# @param N:			Width of matrix
+# @param D0:		Cut-off frequency.  Positive float value
+# @param n:			multiplication value for butterworth, default 1.0 if not set
+#
+#	@return H:		matrix of MxN, where the content of each cell is (1-x)
+def genHPFilter(t, M, N, D0=100.0, n=1.0):
+	Hlp = genLPFilter(t, M, N, D0, n)
+	Hhp = (1 - Hlp)
+	return Hhp
 
 
 def setSPlotSize(arr):
@@ -208,8 +290,21 @@ M = len( oIMG )
 N = len( oIMG[1] )
 
 H = genLPFilter( "ideal", 5, 5, 2.5, 1.0)
+print H
 H = genLPFilter( "btw", 	5, 5, 2.5, 1.0)
+print H
 H = genLPFilter( "gaussian", 	5, 5, 2.5, 1.0)
+print H
+print
+print
+H = genHPFilter( "ideal", 5, 5, 2.5, 1.0)
+print H
+H = genHPFilter( "btw", 	5, 5, 2.5, 1.0)
+print H
+H = genHPFilter( "gaussian", 	5, 5, 2.5, 1.0)
+print H
+print
+print
 
 
 
